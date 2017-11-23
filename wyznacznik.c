0fd4f1daced96c99b16dd27b4fa16d42cc692970
LukaszKur <lukaszkurasz2@gmail.com>
2017-11-23 08:56:00 +0100

    Uklad rownan

---
+++ wyznacznik.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int mnoz(int a,int b) 
+	{
+	return a*b;
+	}
+int dodaj(int a, int b)
+	{
+	return a+b;
+	}
+int odejmij(int a, int b)
+	{
+	return a-b;
+	}
+int dziel(int a, int b)
+	{
+	if (b==0)
+	{
+	printf("Nie dziel cholero przez zero :)\n");
+	}
+	else
+	{
+	return a/b;
+	}
+	}
+int main(void)
+{
+	int a1, a2,b1,c1,c2,W,Wx,Wy;
+	printf("Podja wartosc a1: ");
+	scanf("%i",&a1);
+	printf("Podaj wartosc a2: ");
+	scanf("%i",&a2);
+	printf("Podaj wartosc b1: ");
+	scanf("%i",&b1);
+	printf("Podaj wartosc b2: ");
+	scanf("%i",&b2);
+	printf("Podaj wartosc c1: ");
+	scanf("%",&c1);
