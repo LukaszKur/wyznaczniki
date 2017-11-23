610b4c09f713a793db7ef6f14fdd2f27f2b2091d
LukaszKur <lukaszkurasz2@gmail.com>
2017-11-23 10:01:44 +0100

    Uklad rownan - funcja wczytaj

--- wyznacznik.c
+++ wyznacznik.c
@@ -1,36 +1,23 @@
 #include <stdio.h>
-int mnoz(int a,int b) 
-	{
-	return a*b;
-	}
-int dodaj(int a, int b)
-	{
-	return a+b;
-	}
-int odejmij(int a, int b)
-	{
-	return a-b;
-	}
-int dziel(int a, int b)
-	{
-	if (b==0)
-	{
-	printf("Nie dziel cholero przez zero :)\n");
-	}
-	else
-	{
-	return a/b;
-	}
-	}
-int main(void)
+void wczytaj(double *a1,double *a2,double *b1,double *b2,double *c1,double *c2)
+{	puts("Podja wartosc a1: ");
+        scanf("%lf",a1);
+        puts("Podaj wartosc a2: ");
+        scanf("%lf",a2);
+        puts("Podaj wartosc b1: ");
+        scanf("%lf",b1);
+        puts("Podaj wartosc b2: ");
+        scanf("%lf",b2);
+        puts("Podaj wartosc c1: ");
+        scanf("%lf",c1);
+        puts("Podaj wartosc c2: ");
+        scanf("%lf",c2);
+}
+
+int main()
 {
-	int a1, a2,b1,c1,c2,W,Wx,Wy;
-	printf("Podja wartosc a1: ");
-	scanf("%i",&a1);
-	printf("Podaj wartosc a2: ");
-	scanf("%i",&a2);
-	printf("Podaj wartosc b1: ");
-	scanf("%i",&b1);
-	printf("Podaj wartosc b2: ");
-	scanf("%i",&b2);
-	
+	double a1,a2,b1,b2,c1,c2,W,Wx,Wy;
+	wczytaj (&a1,&a2,&b1,&b2,&c1,&c2);
+
+return 0;
+}
